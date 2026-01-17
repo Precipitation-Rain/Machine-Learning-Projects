cdbf3c400d624ecce6abd2c905eaf36e7d6e205c
Precipitation-Rain <rajvardhanshewale1200@gmail.com>
2026-01-17 12:02:51 +0530

    Handles the some issues like None Crashing

--- 01_SpellCheckerApp/AppWeb.py
+++ 01_SpellCheckerApp/AppWeb.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from spellchecker import SpellChecker
+import string
 
 # Initialize spell checker
 spell = SpellChecker()
@@ -13,27 +14,47 @@ st.write("Enter text below to check spelling mistakes.")
 text = st.text_area("Enter text to check", height=150)
 
 if st.button("Check Spelling"):
+
     if text.strip() == "":
         st.warning("Please enter some text.")
+
     else:
         words = text.split()
         corrected_words = []
         corrections = []
 
         for word in words:
-            lower_word = word.lower()
+
+            #removing punctuations
+            clean_word = word.strip(string.punctuation)
+
+            #handling symbols and numbers
+            if not clean_word.isalpha():
+                corrected_words.append(word)
+                continue
+
+            #skip proper nouns
+
+            lower_word = clean_word.lower()
+
+            if lower_word in spell:
+                corrected_words.append(word)
+                continue
+
             corrected_word = spell.correction(lower_word)
 
-            # If no correction is found, keep the original word
             if corrected_word is None:
                 corrected_words.append(word)
-            else:
-                # If correction is different, note it
-                if corrected_word != lower_word:
-                    corrections.append(f'{word} → {corrected_word}')
-                corrected_words.append(corrected_word)
+                continue
 
-        corrected_text = " ".join(corrected_words).capitalize()
+            if word[0].isupper():
+                corrected_word = corrected_word.capitalize()
+
+            corrections.append(f'{word} -> {corrected_word}')
+            corrected_words.append(corrected_word)
+
+        corrected_text = " ".join(corrected_words)  
+
 
         # Output
         st.subheader("Corrected Text")
